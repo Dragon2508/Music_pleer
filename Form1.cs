3e3b515027273d991f0598fe6e12f3d66a365fa1
Pankratov-Denis <58044575+Pankratov-Denis@users.noreply.github.com>
2020-04-09 12:35:57 +0300

    Add files via upload
    1

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Music_pleer
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer player = null;
+        string fileName = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            player = new SoundPlayer();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenMedia();
+        }
+        private void OpenMedia()
+        {
+            OpenFileDialog oFD = new OpenFileDialog()
+            {
+                Filter = "WAV|*.wav",
+                Multiselect = false,
+                ValidateNames = true,
+            };
+            if (oFD.ShowDialog() == DialogResult.OK)
+            {
+                fileName = oFD.FileName;
+                textBox1.Text = oFD.FileName;
+            }
+        }
+    }
+}
