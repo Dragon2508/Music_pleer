8c534e6f6abdcf87adc1eac04c7cda0567705ed6
Pankratov-Denis <58044575+Pankratov-Denis@users.noreply.github.com>
2020-04-09 12:09:02 +0300

    открытие и пауза

--- Music_pleer/Music_pleer/Form1.cs
+++ Music_pleer/Music_pleer/Form1.cs
@@ -7,12 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace Music_pleer
 {
     public partial class Form1 : Form
     {
-
+        SoundPlayer player = null;
+        string fileName = string.Empty;
         public Form1()
         {
             InitializeComponent();
@@ -20,12 +22,12 @@ namespace Music_pleer
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            player = new SoundPlayer();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            player.Stop();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -35,7 +37,21 @@ namespace Music_pleer
 
         private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
         {
-             
+            OpenMedia();
+        }
+        private void OpenMedia()
+        {
+            OpenFileDialog oFD = new OpenFileDialog()
+            {
+                Filter = "WAV|*.wav",
+                Multiselect = false,
+                ValidateNames = true,
+            };
+            if (oFD.ShowDialog() == DialogResult.OK)
+            {
+                fileName = oFD.FileName;
+                textBox1.Text = oFD.FileName;
+            }
         }
     }
 }
